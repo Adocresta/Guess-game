380cc95bd4454abb7a38e918241786840df3db00
Onur <znygaga004@gmail.com>
2022-05-31 18:03:48 +0300

    implemented turn system for player X and O

--- Tic-Tac-Toe/app.js
+++ Tic-Tac-Toe/app.js
@@ -1,5 +1,5 @@
 const gameBoard = [
-  [null, null, null],
+  ["O", null, null],
   [null, null, null],
   [null, null, null],
 ];
@@ -8,35 +8,82 @@ const gameBoard = [
 let gameOver = false;
 let playerInputOne = null;
 let playerInputTwo = null;
+let isValidInput = false;
 
 while (!gameOver) {
   // *player X Turn
+  console.log("Turn of 'X'");
   // player input for row and column number
 
   // check the number it should be between 1 and 3 (-1 for array logic)
   while (1) {
-    playerInputOne = parseInt(prompt("Enter Row"));
-    playerInputTwo = parseInt(prompt("Enter Column"));
-    console.log(`your inputs are ${playerInputOne} and ${playerInputTwo}`);
-    if (0 < playerInputOne < 4) {
-      console.log("valid input");
-    } else if (0 > playerInputTwo > 3) {
-      console.log("invalin column input!");
+    while (!isValidInput) {
+      playerInputOne = parseInt(prompt("Enter Row"));
+      playerInputTwo = parseInt(prompt("Enter Column"));
+      console.log(`your inputs are ${playerInputOne} and ${playerInputTwo}`);
+      if (playerInputOne < 4 && playerInputTwo < 4) {
+        if (playerInputOne > 0 && playerInputTwo > 0) {
+          playerInputOne--;
+          playerInputTwo--;
+          isValidInput = true;
+          console.log("valid input!");
+          break;
+        }
+        console.log("invalid input!");
+      } else {
+        console.log("invalid input!");
+      }
+    }
+
+    isValidInput = false;
+    // check if the space is not null or isNotOccupied
+    if (gameBoard[playerInputOne][playerInputTwo] === null) {
+      // change the value with X
+      gameBoard[playerInputOne][playerInputTwo] = "X";
+      console.log(gameBoard);
+      break;
     } else {
-      console.log("invalin row input!");
+      console.log("space is taken come again");
     }
   }
 
-  gameBoard[playerInputOne][playerInputTwo] = "X";
-
-  // check if the space is not null or isNotOccupied
-  // change the value with X
   //check rows for 3 matching X end turn
 
-  // O turn
-  //same stuff with O
-  gameBoard[prompt("Enter Row") - 1][prompt("Enter Column") - 1] = "O";
-  console.log(gameBoard);
+  // *Player O turn
+  console.log("Turn of 'O'");
+  while (1) {
+    while (!isValidInput) {
+      playerInputOne = parseInt(prompt("Enter Row"));
+      playerInputTwo = parseInt(prompt("Enter Column"));
+      console.log(`your inputs are ${playerInputOne} and ${playerInputTwo}`);
+      if (playerInputOne < 4 && playerInputTwo < 4) {
+        if (playerInputOne > 0 && playerInputTwo > 0) {
+          playerInputOne--;
+          playerInputTwo--;
+          isValidInput = true;
+          console.log("valid input!");
+          break;
+        }
+        console.log("invalid input!");
+      } else {
+        console.log("invalid input!");
+      }
+    }
+
+    isValidInput = false;
+    // check if the space is not null or isNotOccupied
+    if (gameBoard[playerInputOne][playerInputTwo] === null) {
+      // change the value with X
+      gameBoard[playerInputOne][playerInputTwo] = "O";
+      console.log(gameBoard);
+      break;
+    } else {
+      console.log("space is taken come again");
+    }
+    //same stuff with O
+    console.log(gameBoard);
+  }
+  //check rows for 3 matching O end turn
 }
 
 console.log(gameBoard);
